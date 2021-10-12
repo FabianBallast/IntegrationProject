9f9c0ff3ead667db4191fbf78cdf76623a1e1cbe
Fabian <fabian.knl.ballast@gmail.com>
2021-10-12 10:41:36 +0200

    Robust musyn controller

--- robustControlCT.m
+++ robustControlCT.m
@@ -6,9 +6,15 @@ load data/constPar/lin_model_ct
 % C= C(1, 1:2);
 % D = D(1);
 
-sys = ss(A,B,C,D);
-sys.InputName='u';
-sys.OutputName={'theta_1', 'theta_2'};
+sys_nom = ss(A,B,C,D);
+sys_nom.InputName='u';
+sys_nom.OutputName={'theta_1', 'theta_2'};
+
+Wunc = makeweight(0.1,60,10);
+unc = ultidyn('unc',[1 1],'SampleStateDim',5);
+sys_rob = sys_nom*(1 + Wunc*unc);
+sys_rob.InputName='u';
+sys_rob.OutputName={'theta_1', 'theta_2'};
 %%
 ReferenceTarget1 = 1.5*tf([1 0.001], [1 0.5]);
 ReferenceTarget2 = 1.2*tf([1 0.01], [1 10]);
@@ -33,14 +39,12 @@ off1  = sumblk('off1 = r_theta1 - theta_1');
 off2  = sumblk('off2 = -r_theta1 - theta_2');
 ICinputs = {'r_theta1';'u'};
 ICoutputs = {'z1';'z2';'z3';'off1'; 'off2'};
-qcaric = connect(sys,Wref1,Wref2, Wcost, off1,off2,ICinputs,ICoutputs)
-
+qpend_nom = connect(sys_nom,Wref1,Wref2, Wcost, off1,off2,ICinputs,ICoutputs);
+qpend_rob = connect(sys_rob,Wref1,Wref2, Wcost, off1,off2,ICinputs,ICoutputs);
 %%
-[K,~,gamma] = hinfsyn(qcaric,2,1);
+[K,~,gamma] = hinfsyn(qpend_nom,2,1);
 gamma
 
-% [K,muPerf] = musyn(qcaric,1,1);
-% muPerf
 K.InputName = {'off1', 'off2'};
 K.OutputName = 'u';
 
@@ -56,10 +60,15 @@ step(sys_cl, 1, opt)
 %%
 bodemag(sys_cl)
 %% 
-K_nom = tf(-5);
-K_nom.InputName = {'off1'}; %, 'off2'};
-K_nom.OutputName = 'u';
+[K,muPerf] = musyn(qpend_rob,2,1);
+muPerf
+%%
+K.u = {'off1', 'off2'};
+K.y = 'u';
+ICinputs = {'r_theta1'};
+ICoutputs = {'off1'; 'off2'};
+sys_cl = connect(qpend_nom,K,ICinputs,ICoutputs);
+step(sys_cl, 1, opt)
 
-sys_cl = connect(qcaric, K_nom, 'r_theta1', {'z1', 'u', 'off1'});%, 'off2'});sys_cl = connect(qcaric, K, 'r_theta1', {'z1', 'u', 'off1'});%, 'off2'});
-
-step(sys_cl);
+%%
+save data/controller/musyn_controller K sys_cl
