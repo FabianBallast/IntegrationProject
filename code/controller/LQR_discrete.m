70dae83d8af061786563a71443fe4536c11995c8
Fabian <fabian.knl.ballast@gmail.com>
2021-10-18 22:02:16 +0200

    LQR simulation improved

--- code/controller/LQR_discrete.m
+++ code/controller/LQR_discrete.m
@@ -9,7 +9,7 @@ load ../../data/constPar/lin_model_dt
 % Qd = 1*diag([100 0.0001 150 0.01]);
 % Qd = 1*diag([100 0.0001 150 0.0001]); Rd = 200;
 
-Qd = 1*diag([150 0.0001 150 0.0001]); Rd = 75; % 75 for better tracking, 100 for smooth movement
+Qd = 1*diag([150 0.0001 150 0.0001]); Rd = 100; % 75 for better tracking, 100 for smooth movement
 
 % Rd = 1;
 
@@ -17,33 +17,7 @@ Kd = dlqr(sysd.A, sysd.B, Qd, Rd)
 
 save ../../data/constPar/LQR_gain_d Kd sysd Qd Rd
 
-%% Make ready for simulation
-load ../../data/constPar/model_parameters
-load ../../data/constPar/motorPar2
-
-model_par.c2 = c2_val;
-model_par.b2 = b2_val;
-model_par.J2 = J2_val;
-model_par.g = g_val;
-model_par.l1 = l1_val;
-model_par.m2 = m2_val;
-
-model_par.a11 = a11;
-model_par.a12 = a12;
-model_par.a22 = c2_val*g_val*m2_val/(m2_val*c2_val^2+J2_val);
-
-Theta1_0 = 0;
-Thetad1_0 = 0;
-Theta2_0 = 0.01;
-Thetad2_0 = 0;
-
 h = 0.01;
-
-obs_poles_d = exp([-80 -20 -5 -4]*h);
-% obs_poles_d = [0.55 0.85 0.95 0.96];
-poles_d = exp([-10.8860 -44.1060 -2.0000 -0.5000]*h);
-% Kd = place(sysd.A, sysd.B, poles_d);
-Ld = place(sysd.A',sysd.C',obs_poles_d).';
 
 %% Kalman filter
 
@@ -52,6 +26,7 @@ load ../../data/sysID/noise
 Q = 0.0000018;
 R = cov(theta_1, theta_2);
 [kalmf,Ld,~,Mx,Z] = kalman(sysd, Q, R);
+
 
 
 
---
+++ code/controller/LQR_sim_discrete_mat_test.m
@@ -0,0 +1,96 @@
+%% Setup
+clear; clc;
+
+%% Load linear model
+load ../../data/constPar/lin_model_dt
+
+%% Make ready for simulation
+load ../../data/constPar/model_parameters
+load ../../data/constPar/motorPar2
+
+model_par.c2 = c2_val;
+model_par.b2 = b2_val;
+model_par.J2 = J2_val;
+model_par.g = g_val;
+model_par.l1 = l1_val;
+model_par.m2 = m2_val;
+
+model_par.a11 = a11;
+model_par.a12 = a12;
+model_par.a22 = c2_val*g_val*m2_val/(m2_val*c2_val^2+J2_val);
+
+Theta1_0 = 0;
+Thetad1_0 = 0;
+Theta2_0 = 0;
+Thetad2_0 = 0;
+
+h = 0.01;
+%% Kalman filter
+
+load ../../data/sysID/noise
+
+Q = 0.0000018;
+R = cov(theta_1, theta_2);
+[kalmf,Ld,~,Mx,Z] = kalman(sysd, Q, R);
+
+
+%% Create cost matrices
+% Order of states: th1 thd1 th2 thd2
+
+% Set 1
+% Q1 = diag([1,1,1,1]); R1 = 1;
+% Q2 = diag([10, 1, 10, 1]); R2 = 1;
+% Q3 = diag([1, 10, 1, 10]); R3 = 1;
+
+% Set 2
+Q1 = diag([10, 1, 10, 1]); R1 = 1;
+Q2 = diag([100,1,100,1]); R2 = 1;
+Q3 = diag([100, 0.1, 100, 0.1]); R3 = 1;
+
+K1 = dlqr(sysd.A, sysd.B, Q1, R1);
+K2 = dlqr(sysd.A, sysd.B, Q2, R2);
+K3 = dlqr(sysd.A, sysd.B, Q3, R3);
+
+K = [K1; K2; K3];
+
+%% Reference
+t = 0:0.01:57;
+
+% Step (trapezoidal) wave
+% u = smooth(square(t*2*pi / 20), 100);
+
+% Sin wave
+u = sin(t*2*pi / 10).';
+
+t = 0:0.01:60;
+u = [zeros(300, 1); u];
+plot(t, u);
+
+ref.time = t.';
+ref.signals.values = smooth(u.', 200);
+%% Run simulations
+
+for i = 1:length(K(:, 1))
+    Kd = K(i, :);
+    sim('../../simulink/LQR_sim_discrete');
+    t_arr(:,i) = theta_2.Time();
+    th2_arr(:,i) = theta_2.Data();
+end
+
+%%
+figure(1); clf;
+hold all;
+for i =1:length(K(:, 1))
+   plot(t_arr(:, i), th2_arr(:, i)); 
+end
+
+plot(Reference.Time(), Reference.Data(:, 3), 'k--');
+hold off;
+ylim([-1, 1]);
+xlabel('Time (s)');
+ylabel('\theta_2 (rad)')
+legend('K_3', 'K_4', 'K_5', 'Reference');
+title('Square wave reference with different cost matrices');
+
+%%
+
